0e6feedde7b36107848870fddf34b7f9e0b200da
User <User@COLIN>
2025-05-14 15:51:13 +0200

    initial Commits

---
+++ FussballApp/Profile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FussballApp
+{
+    class Profile
+    {
+        public List<League> FavoriteLeagues { get; set; } = new List<League>();
+        public List<Team> FavoriteTeams { get; set;} = new List<Team>();
+
+        Profile()
+        {
+
+        }
+
+        public void AddLeague(League league)
+        {
+            FavoriteLeagues.Add(league);
+        }
+        public void RemoveLeague(League league)
+        {
+            FavoriteLeagues.Remove(league);
+        }
+
+        public void AddTeam(Team team)
+        {
+            FavoriteTeams.Add(team);
+        }
+        public void RemoveTeam(Team team)
+        {
+            FavoriteTeams.Remove(team);
+        }
+    }
+}
