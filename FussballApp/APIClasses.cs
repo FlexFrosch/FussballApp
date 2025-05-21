3a08ce8147bec0d3db386c225894249ed361692b
User <User@COLIN>
2025-05-21 16:01:24 +0200

    initial commit

---
+++ FussballApp/APIClasses.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FussballApp
+{
+ 
+        public class RootObject
+        {
+            public int Count { get; set; }
+            public Filters Filters { get; set; }
+            public List<Competition> Competitions { get; set; }
+        }
+
+        public class Filters
+        {
+            public string Client { get; set; }
+        }
+
+        public class Competition
+        {
+            public int Id { get; set; }
+            public Area Area { get; set; }
+            public string Name { get; set; }
+            public string Code { get; set; }
+            public string Type { get; set; }
+            public string Emblem { get; set; }
+            public string Plan { get; set; }
+            public Season CurrentSeason { get; set; }
+            public int NumberOfAvailableSeasons { get; set; }
+            public DateTime LastUpdated { get; set; }
+        }
+
+        public class Area
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Code { get; set; }
+            public string Flag { get; set; }
+        }
+
+        public class Season
+        {
+            public int Id { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public int CurrentMatchday { get; set; }
+            public object Winner { get; set; } // kann auch null sein oder eine Klasse, wenn mehr Infos gebraucht werden
+        }
+
+    
+}
---
+++ FussballApp/Reader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace FussballApp
+{
+    public class API_Reader
+    {
+        private static readonly string apiKey = "f1e793d865804096a476f8052af2587f";
+        private static readonly string apiUrl = "https://api.football-data.org/v4/competitions";
+
+        public static async Task GetCompetitions()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Auth-Header setzen
+                client.DefaultRequestHeaders.Add("X-Auth-Token", "f1e793d865804096a476f8052af2587f");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    var result = JsonConvert.DeserializeObject<RootObject>(responseBody);
+                    // JSON parsen
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Fehler: {e.Message}");
+                }
+            }
+        }
+    }
+}
