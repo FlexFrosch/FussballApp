42f1104c1015d17981aea6a2c62d25976a516942
Jabobe17 <jakob.seeberger@aon.at>
2025-05-28 17:06:38 +0200

    GUI

--- FussballApp/MainWindow.xaml.cs
+++ FussballApp/MainWindow.xaml.cs
@@ -20,7 +20,43 @@ namespace FussballApp
         public MainWindow()
         {
             InitializeComponent();
-            Debug.Write("Test Commit");
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.ResizeMode = ResizeMode.CanResize;
+                this.WindowState = WindowState.Normal;
+            }
+
+            base.OnKeyDown(e);
+        }
+
+        private void ShowHome(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowLeagues(object sender, RoutedEventArgs e)
+        {
+            LigenWindow window = new LigenWindow();
+            window.Show();
+            this.Close();       
+        }
+
+        private void ShowProfile(object sender, RoutedEventArgs e)
+        {
+            ProfileWindow window = new ProfileWindow();
+            window.Show();
+            this.Close();
+
+        }
+
+        private void MatchesGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 
